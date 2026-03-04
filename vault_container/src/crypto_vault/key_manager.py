10fb786b53005809cbbc21dcd9a1b0c81b09f613
Edgar Salazar <edgar.salazars@ingenieria.unam.edu>
2026-03-04 22:12:45 +0000

    Add KeyManager class for key generation and loading

---
+++ vault_container/src/crypto_vault/key_manager.py
@@ -0,0 +1,37 @@
+# src/crypto_vault/key_manager.py
+# Clase key_manager para generar y cargar llaves
+# Version 1.0
+
+import os
+import base64
+
+class KeyManager:
+    @staticmethod
+    def generate_key_file(path: str) -> bytes:
+        """Genera una llave de 256 bits y la guarda en un archivo.
+
+        Args:
+            path (str): La ruta donde se guardará la llave.
+        Returns:
+            bytes: La llave generada.
+        """
+        key = base64.urlsafe_b64encode(os.urandom(32))  # 256 bits
+        with open(path, "wb") as f:
+            f.write(key)
+        return key
+
+    @staticmethod
+    def load_key_file(path: str) -> bytes:
+        """Carga una llave de 256 bits desde un archivo.
+        
+        Args:
+            path (str): La ruta donde se encuentra la llave.
+        Returns:
+            bytes: La llave cargada.
+        """
+        #lectura en modo binario (lee bytes)
+        with open(path, "rb") as f:
+            key = f.read()
+        if len(key) != 32:
+            raise ValueError("La llave debe ser de 256 bits (32 bytes).")
+        return key
